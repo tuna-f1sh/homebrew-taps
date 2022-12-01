6d7399647cc1be02e1cf6de5214ef5f7cdc97b24
John Whittington <git@jbrengineering.co.uk>
2022-12-01 11:56:33 +0100

    release update

--- cyme.rb
+++ cyme.rb
@@ -1,9 +1,9 @@
 class Cyme < Formula
-  desc "Port of lsusb to macOS using system_profiler output"
+  desc "List system USB buses and devices; a modern and compatiable `lsusb`"
   homepage "https://github.com/tuna-f1sh/cyme"
-  url "https://github.com/tuna-f1sh/cyme/archive/refs/tags/0.1.1.tar.gz"
-  version "0.1.1"
-  sha256 "77aee59a6f26fdf5cdcc7196c3fd88e1c1e022dbdde48e29827eef4c309a9b7f"
+  url "https://github.com/tuna-f1sh/cyme/archive/refs/tags/0.7.8.tar.gz"
+  version "0.7.8"
+  sha256 "ec11f3314257dfba743e56f9c4534425af39ec6e1088095ccd197373d28f0be5"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
