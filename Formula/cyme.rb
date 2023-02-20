92c8208e1df141d0dc5b2540c645a4ae005469d0
John Whittington <git@jbrengineering.co.uk>
2023-02-20 18:06:13 +0100

    cyme 1.2.8
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/cyme.rb
+++ Formula/cyme.rb
@@ -1,9 +1,9 @@
 class Cyme < Formula
   desc "List system USB buses and devices; a modern and compatiable `lsusb`"
   homepage "https://github.com/tuna-f1sh/cyme"
-  url "https://github.com/tuna-f1sh/cyme/releases/download/v1.2.7/cyme-v1.2.7-x86_64-apple-darwin.tar.gz"
-  version "1.2.7"
-  sha256 "973cb035a9460be94002c00dcfec34c6ddec18c73b52b908f24fdd8ef4b84e79"
+  url "https://github.com/tuna-f1sh/cyme/releases/download/v1.2.8/cyme-v1.2.8-x86_64-apple-darwin.tar.gz"
+  version "1.2.8"
+  sha256 "38201ddef9d1f47d6474e47d3e2aa6945fc490d3e0599802eab2f3d2d768b4d0"
   license "GPL-3.0-or-later"
   depends_on "libusb"
 
