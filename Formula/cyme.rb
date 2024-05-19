d6c0539113a14c490f79dfd6ab96661a2f9d1c87
John Whittington <git@jbrengineering.co.uk>
2024-05-19 09:36:27 +0200

    remove feedback from brew audit

--- Formula/cyme.rb
+++ Formula/cyme.rb
@@ -2,7 +2,6 @@ class Cyme < Formula
   desc "List system USB buses and devices; a modern and compatiable `lsusb`"
   homepage "https://github.com/tuna-f1sh/cyme"
   url "https://github.com/tuna-f1sh/cyme/releases/download/v1.6.0/cyme-v1.6.0-universal-apple-darwin.tar.gz"
-  version "1.6.0"
   sha256 "284abe1de803e168d9ac658115a8f3db677a087fea5f38ae6cd328ab3bcd1990"
   license "GPL-3.0-or-later"
   depends_on "libusb"
@@ -13,7 +12,7 @@ class Cyme < Formula
     bash_completion.install "./autocomplete/cyme.bash"
     zsh_completion.install "./autocomplete/_cyme"
     fish_completion.install "./autocomplete/cyme.fish"
-    ohai "For an 'lsusb' command using cyme, add \`alias lsusb='cyme --lsusb'\` to one's shell startup"
+    ohai "For an 'lsusb' command using cyme, add `alias lsusb='cyme --lsusb'` to one's shell startup"
   end
 
   test do
